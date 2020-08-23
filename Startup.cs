7c7e979cc218003c6d1705e5a793c68fa57985e2
dspitz <devin.spitz@duagon.com>
2020-08-23 22:24:34 +0200

    revoke last commit

--- Startup.cs
+++ Startup.cs
@@ -80,6 +80,7 @@ namespace PavlovRconWebserver
          app.UseRouting();
          app.UseAuthentication();
          app.UseAuthorization();
+         app.UseHttpsRedirection();
          app.UseEndpoints(endpoints =>
          {
             endpoints.MapDefaultControllerRoute();
