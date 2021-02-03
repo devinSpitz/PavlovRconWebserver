6028f97250a62be8339edb28e037920cfc175d73
Devin Spitz <devin@spitz.onl>
2021-02-03 18:10:00 +0100

    Quick and dirty fix for kick && small fix when only one player is on the server for the player list

---
+++ Models/KickViewModel.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+
+namespace PavlovRconWebserver.Models
+{
+   public class KickViewModel
+   {
+      public bool Kick { get; set; } = false;
+
+      public string UniqueID { get; set; } = "";
+   }
+}
