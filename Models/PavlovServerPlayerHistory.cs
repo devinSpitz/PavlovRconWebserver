6cf0582ff76924694bc3e1ac2472fce517e3e7b2
devinSpitz <devin@spitz.onl>
2021-02-27 13:06:49 +0100

    - WIP Match making - History faster delete there will be some sort of stats per player saved at a later time.

--- Models/PavlovServerPlayerHistory.cs
+++ Models/PavlovServerPlayerHistory.cs
@@ -1,12 +1,13 @@
 using System;
 using System.ComponentModel.DataAnnotations.Schema;
+using LiteDB;
 
 namespace PavlovRconWebserver.Models
 {
     public class PavlovServerPlayerHistory : PavlovServerPlayer
     {
         
-        public int Id { get; set; }
+        public ObjectId Id { get; set; }
         public DateTime date { get; set; }
     }
 }
