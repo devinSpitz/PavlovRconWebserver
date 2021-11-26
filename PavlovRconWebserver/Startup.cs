925a82d0f230fe9424e74551f33282bb56f4fa3d
dspitz <devin.spitz@duagon.com>
2021-11-26 10:39:07 +0100

    chrome fix?

--- PavlovRconWebserver/Startup.cs
+++ PavlovRconWebserver/Startup.cs
@@ -45,9 +45,15 @@ namespace PavlovRconWebserver
             {
                 options.MinimumSameSitePolicy = SameSiteMode.Unspecified;
                 options.OnAppendCookie = cookieContext =>
-                    CheckSameSite(cookieContext.Context, cookieContext.CookieOptions);
+                {
+                    cookieContext.CookieOptions.SameSite = SameSiteMode.None;
+                    cookieContext.CookieOptions.Secure = true;
+                };
                 options.OnDeleteCookie = cookieContext =>
-                    CheckSameSite(cookieContext.Context, cookieContext.CookieOptions);
+                {
+                    cookieContext.CookieOptions.SameSite = SameSiteMode.None;
+                    cookieContext.CookieOptions.Secure = true;
+                };
             });
             services.AddHangfire(x => x.UseMemoryStorage());
             services.AddHangfireServer(x => { x.WorkerCount = 10; });
@@ -105,60 +111,6 @@ namespace PavlovRconWebserver
                 config.Position = Position.Right;
                 config.Gravity = Gravity.Top;
             });
-        }
-        public static bool DisallowsSameSiteNone(string userAgent)
-        {
-            // Check if a null or empty string has been passed in, since this
-            // will cause further interrogation of the useragent to fail.
-            if (string.IsNullOrWhiteSpace(userAgent))
-                return false;
-    
-            // Cover all iOS based browsers here. This includes:
-            // - Safari on iOS 12 for iPhone, iPod Touch, iPad
-            // - WkWebview on iOS 12 for iPhone, iPod Touch, iPad
-            // - Chrome on iOS 12 for iPhone, iPod Touch, iPad
-            // All of which are broken by SameSite=None, because they use the iOS networking
-            // stack.
-            if (userAgent.Contains("CPU iPhone OS 12") ||
-                userAgent.Contains("iPad; CPU OS 12"))
-            {
-                return true;
-            }
-
-            // Cover Mac OS X based browsers that use the Mac OS networking stack. 
-            // This includes:
-            // - Safari on Mac OS X.
-            // This does not include:
-            // - Chrome on Mac OS X
-            // Because they do not use the Mac OS networking stack.
-            if (userAgent.Contains("Macintosh; Intel Mac OS X 10_14") &&
-                userAgent.Contains("Version/") && userAgent.Contains("Safari"))
-            {
-                return true;
-            }
-
-            // Cover Chrome 50-69, because some versions are broken by SameSite=None, 
-            // and none in this range require it.
-            // Note: this covers some pre-Chromium Edge versions, 
-            // but pre-Chromium Edge does not require SameSite=None.
-            if (userAgent.Contains("Chrome/5") || userAgent.Contains("Chrome/6"))
-            {
-                return true;
-            }
-
-            return false;
-        }
-        private void CheckSameSite(HttpContext httpContext, CookieOptions options)
-        {
-            if (options.SameSite == SameSiteMode.None)
-            {
-                var userAgent = httpContext.Request.Headers["User-Agent"].ToString();
-                if (DisallowsSameSiteNone(userAgent))
-                {
-                    options.SameSite = SameSiteMode.Unspecified;
-                    options.Secure = true;
-                }
-            }
         }
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
